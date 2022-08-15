c673e1698ba4909f2a7575dd47a6cb411bad8a66
nabendu82 <nabendu.biswas@gmail.com>
2022-08-15 21:47:23 +0530

    Day 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import EventBind from './components/EventBind';
+// import Counter from './components/Counter';
+// import { Greet, ReactGreet } from './components/Greet';
+// import Greeting from './components/Greeting';
+// import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Greet />
+      <ReactGreet />*/}
+      {/* <Welcome name="Flutter">Language for cross mobile development</Welcome> 
+      <Welcome name="React Native" /> 
+      <Welcome name="Java"><button>Click</button></Welcome>
+      <hr />
+      <Greeting name="JavaScript">Language of the Web</Greeting>
+      <Greeting name="Golang" />
+      <Greeting name="Swift">Language for iOS app</Greeting> */}
+      {/* <Counter /> */}
+      <EventBind />
     </div>
   );
 }
