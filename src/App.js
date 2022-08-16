3732ef08a2c56cdc992bec24247519592dd58a4d
nabendu82 <nabendu.biswas@gmail.com>
2022-08-16 21:44:40 +0530

    Day 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import './App.css';
-import EventBind from './components/EventBind';
-// import Counter from './components/Counter';
-// import { Greet, ReactGreet } from './components/Greet';
-// import Greeting from './components/Greeting';
-// import Welcome from './components/Welcome';
+import RefsDemo from './components/RefsDemo';
 
 function App() {
   return (
     <div className="App">
-      {/* <Greet />
-      <ReactGreet />*/}
-      {/* <Welcome name="Flutter">Language for cross mobile development</Welcome> 
-      <Welcome name="React Native" /> 
-      <Welcome name="Java"><button>Click</button></Welcome>
-      <hr />
-      <Greeting name="JavaScript">Language of the Web</Greeting>
-      <Greeting name="Golang" />
-      <Greeting name="Swift">Language for iOS app</Greeting> */}
-      {/* <Counter /> */}
-      <EventBind />
+      <RefsDemo />
     </div>
   );
 }
